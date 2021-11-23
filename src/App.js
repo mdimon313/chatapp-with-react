cff1f25a13c4fa7ac915bdc195f49f0fd6b3cdb5
imon313 <imon26362@gmail.com>
2021-11-23 16:53:15 +0600

    chatapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./components/Chat/Login";
+import ChatBox from "./components/Chat/ChatBox";
+import Nav from "./components/Chat/Nav";
+import Reg from "./components/Chat/Reg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/enroll" element={<Reg />} />
+        <Route path="/chat" element={<ChatBox />} />
+        {/* // <Login /> */}
+        {/* <Reg /> */}
+      </Routes>
+    </>
   );
 }
 
